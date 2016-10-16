37b0f85438cb8b22b9a4345a76919312f930ebeb
JellyDoge <wither1130@gmail.com>
2016-10-15 21:09:25 -0400

    settin up stuff part 2
    suprise it was forge all along

--- src/main/java/dogencam/stonected/stonected.java
+++ src/main/java/dogencam/stonected/stonected.java
@@ -2,7 +2,12 @@ package dogencam.stonected;
 
 import dogencam.stonected.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = reference.MOD_ID, name = reference.Name, version = reference.VERSION, acceptedMinecraftVersions = reference.ACCEPTED_VERSIONS)
 public class stonected {
@@ -10,6 +15,24 @@ public class stonected {
 	@Instance
 	public static stonected instance;
 	
-	@SidedProxy()
+	@SidedProxy(clientSide = reference.CLIENT_PROXY_CLASS, serverSide = reference.SERVER_PROXY_CLASS)
 	public static CommonProxy proxy;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		System.out.println("Pre Init");
+	}
+	
+	@EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+		System.out.println("Init");
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		System.out.println("Post Init");
+	}
 }
